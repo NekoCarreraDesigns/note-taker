f94d8de7b950b30d5dad57aa69fb602a25be6568
NekoCarreraDesigns <denvermaas@gmail.com>
2020-06-13 16:36:18 -0600

    added script tag to the html files

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const express = require("express");
-const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 6000
+const PORT = 5500
 global.appRoot = path.resolve(__dirname);
 
 app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
-
-function handleRequest(req, res) {
-    res.end("The Darkhorse is coming")
-};
-
-const server = http.createServer(handleRequest);
-
-server.listen(PORT, function () {
-    console.log("I'm listening Nick")
-});
 
 app.get("/", (req, res) => {
     res.sendfile(path.join(appRoot) + "/public/", "index.html")
@@ -32,4 +21,10 @@ app.get("/api/notes", (req, res) => {
         return data;
     })
     return res.json(JSON.parse(notes));
+});
+
+
+
+app.listen(PORT, function () {
+    console.log("I'm listening on" + PORT)
 });
